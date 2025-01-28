bd079ab90e56a7893f154569a54fc13720944e76
Poonyawatt Klumnaim <poon@pnwttklm.com>
2025-01-28 23:11:40 +0700

    feat: implement 404

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
-import type { Metadata } from "next";
+"use client";
 import localFont from "next/font/local";
 import "./globals.css";
-import Nav from "./nav";
+import Nav from "@/components/nav";
+import { Square, Tally1, X } from "lucide-react";
+import Bar from "@/components/bar";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const myFont = localFont({
   src: [
@@ -10,16 +14,12 @@ const myFont = localFont({
   ],
 });
 
-export const metadata: Metadata = {
-  title: "ICTPC | Desktop",
-  description: "ICTxDST Sport Games 2025 Programming Competition",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <head>
@@ -29,7 +29,36 @@ export default function RootLayout({
       </head>
       <body className={` ${myFont.className} antialiased`}>
         <Nav />
-        {children}
+        <div className="min-h-screen relative">
+          <Bar />
+          {pathname !== "/" && (
+            <div className="flex items-center w-full justify-between p-1 bg-white">
+              <div></div>
+              <h1 className="text-md font-bold text-gray-700"></h1>
+              <div className="flex items-center text-gray-500 space-x-3">
+                <h1 className="text-md font-bold bg-gray-200 p-1 -rotate-90 rounded-full">
+                  <Tally1 size={16} />
+                </h1>
+                <h1 className="text-md font-bold bg-gray-200 p-1 rounded-full">
+                  <Square size={16} />
+                </h1>
+                <Link href="/">
+                  <h1 className="text-md font-bold bg-gray-200 p-1 rounded-full">
+                    <X size={16} />
+                  </h1>
+                </Link>
+              </div>
+            </div>
+          )}
+          {children}
+          <footer className="absolute bottom-32 ml-2 lg:ml-32 items-center w-full lg:bottom-2 text-xs text-gray-500">
+            <p>
+              Copyright © 2025. Dev Club, in association with Faculty of
+              Information and Communication Technology, Mahidol University. All
+              rights reserved.
+            </p>
+          </footer>
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,13 @@
-"use client";
-
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
-import { motion } from "framer-motion";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
+import { AppIcon } from "@/components/appicon";
+import Bar from "@/components/bar";
+import type { Metadata } from "next";
 
-const AppIcon = ({
-  name,
-  href,
-  icon,
-}: {
-  name: string;
-  href: string;
-  icon: React.ReactNode;
-}) => (
-  <TooltipProvider>
-    <Tooltip>
-      <TooltipTrigger>
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-          className="flex flex-col items-center justify-center w-20 h-20 m-4 hover:bg-[#FFFFFF29] rounded-xl hover:shadow-xl"
-        >
-          <Link href={href}>
-            <div className="flex flex-col items-center p-2">
-              <div className="text-3xl mb-2 text-orange-500">{icon}</div>
-              <p className="text-sm font-medium text-gray-300">{name}</p>
-            </div>
-          </Link>
-        </motion.div>
-      </TooltipTrigger>
-      <TooltipContent>{name}</TooltipContent>
-    </Tooltip>
-  </TooltipProvider>
-);
+export const metadata: Metadata = {
+  title: "ICTPC | Desktop",
+  description: "ICTxDST Sport Games 2025 Programming Competition",
+};
 
 export default function Home() {
   return (
@@ -70,19 +38,11 @@ export default function Home() {
             <Image src="/devclub_logo.svg" width={50} height={50} alt="logo" />
           }
         />
-        {/* <AppIcon
-          name="Google"
-          href="https://google.com"
-          icon={<span>🔍</span>}
-        />
-        <AppIcon name="Terminal" href="#" icon={<span>🖥️</span>} />
-        <AppIcon name="Files" href="#" icon={<span>📂</span>} />
-        <AppIcon name="Store" href="#" icon={<span>🛍️</span>} />
-        <AppIcon name="LibreOffice" href="#" icon={<span>📄</span>} />
-        <AppIcon name="Settings" href="#" icon={<span>⚙️</span>} /> */}
       </main>
 
-      <div className="items-center justify-center flex flex-col space-y-2 mt-4 lg:mt-64">
+      <Bar />
+
+      <div className="items-center justify-center flex flex-col space-y-4 mt-4 lg:mt-64">
         <Image
           src="/logo_w_l.svg"
           width={500}
@@ -93,28 +53,14 @@ export default function Home() {
         <h2 className="text-md flex flex-row gap-3">
           <p className="font-bold">Register Date:</p> Today - Jan 31
         </h2>
-        <h2 className="text-md flex flex-row gap-3">
+        {/* <h2 className="text-md flex flex-row gap-3">
           <p className="font-bold">Practice Session:</p> Mon Feb 10 16:00 IT210
         </h2>
         <h2 className="text-md flex flex-row gap-3">
           <p className="font-bold">Competition Session:</p> Sat Feb 15 13:00
           IT210
-        </h2>
+        </h2> */}
       </div>
-
-      {/* Launcher Bar */}
-      <div className="absolute bottom-3 lg:bottom-0 rounded-3xl lg:rounded-none lg:left-0 lg:h-full flex flex-row lg:flex-col max-w-full items-center lg:space-y-1 lg:pt-6 bg-gray-800 bg-opacity-65  shadow-lg">
-        <AppIcon name="Home" href="/" icon={<span>🏠</span>} />
-        <AppIcon name="Register" href="/regis" icon={<span>📝</span>} />
-        <AppIcon name="Help" href="/contact" icon={<span>❓</span>} />
-      </div>
-
-      <footer className="absolute bottom-36 lg:bottom-2 text-xs text-gray-500">
-        <p>
-          Copyright © 2025. Dev Club, in association with Faculty of Information
-          and Communication Technology, Mahidol University. All rights reserved.
-        </p>
-      </footer>
     </div>
   );
 }
