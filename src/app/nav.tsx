eec6f1f840ea2c91c11e506dec9f9702ec857351
Poonyawatt Klumnaim <poon@pnwttklm.com>
2025-01-28 21:29:45 +0700

    feat: homepage

---
+++ src/app/nav.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Network, Power, Volume2 } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function Nav() {
+  const [dateTime, setDateTime] = useState(getDateTime());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDateTime(getDateTime());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function getDateTime() {
+    const now = new Date();
+    const date = now.getDate();
+    const month = now.toLocaleString("en-US", { month: "short" });
+    const hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    return `${month} ${date} ${hours}:${minutes}`;
+  }
+  return (
+    <header className="flex items-center justify-between w-full p-1 bg-[#070419]  shadow-md">
+      <div></div>
+      <h1 className="text-md font-medium text-white">{dateTime}</h1>
+      <div className="flex items-center text-white space-x-3">
+        <Network size={16} />
+        <Volume2 size={16} />
+        <Power size={16} />
+      </div>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,119 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const AppIcon = ({
+  name,
+  href,
+  icon,
+}: {
+  name: string;
+  href: string;
+  icon: React.ReactNode;
+}) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="flex flex-col items-center justify-center w-20 h-20 m-4 hover:bg-[#FFFFFF29] rounded-xl hover:shadow-xl"
+        >
+          <Link href={href}>
+            <div className="flex flex-col items-center p-2">
+              <div className="text-3xl mb-2 text-orange-500">{icon}</div>
+              <p className="text-sm font-medium text-gray-300">{name}</p>
+            </div>
+          </Link>
+        </motion.div>
+      </TooltipTrigger>
+      <TooltipContent>{name}</TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="min-h-screen bg-gradient-to-br from-[#19B3C5] via-[#070419] to-[#070419] flex flex-col items-center text-white relative">
+      {/* App Icons */}
+      <main className="flex flex-wrap justify-center items-center max-w-5xl">
+        <AppIcon name="Register" href="/regis" icon={<span>📝</span>} />
+        <AppIcon name="About" href="/about" icon={<span>ℹ️</span>} />
+        <AppIcon name="Rules" href="/contest" icon={<span>🖥️</span>} />
+        {/* <AppIcon name="Score" href="/leaderboard" icon={<span>📊</span>} /> */}
+        <AppIcon name="Schedule" href="/schedule" icon={<span>🗓️</span>} />
+        <AppIcon
+          name="OpenChat"
+          href="https://line.me/ti/g2/-V3smyrlmrL1f4b1_mL6m_PGPno6BebmRHlWIQ?utm_source=invitation&utm_medium=link_copy&utm_campaign=default"
+          icon={<Image src="/openchat.svg" width={50} height={50} alt="logo" />}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <AppIcon
+          name="ICTSA"
+          href="https://www.instagram.com/muict_studentactivities?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+          icon={
+            <Image src="/ictsa_logo.jpg" width={50} height={50} alt="logo" />
+          }
+        />
+        <AppIcon
+          name="Dev_Club"
+          href="https://muictdev.club"
+          icon={
+            <Image src="/devclub_logo.svg" width={50} height={50} alt="logo" />
+          }
+        />
+        {/* <AppIcon
+          name="Google"
+          href="https://google.com"
+          icon={<span>🔍</span>}
+        />
+        <AppIcon name="Terminal" href="#" icon={<span>🖥️</span>} />
+        <AppIcon name="Files" href="#" icon={<span>📂</span>} />
+        <AppIcon name="Store" href="#" icon={<span>🛍️</span>} />
+        <AppIcon name="LibreOffice" href="#" icon={<span>📄</span>} />
+        <AppIcon name="Settings" href="#" icon={<span>⚙️</span>} /> */}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <div className="items-center justify-center flex flex-col space-y-2 mt-4 lg:mt-64">
+        <Image
+          src="/logo_w_l.svg"
+          width={500}
+          height={500}
+          alt="logo"
+          className="w-9/12 lg:w-3/12"
+        ></Image>
+        <h2 className="text-md flex flex-row gap-3">
+          <p className="font-bold">Register Date:</p> Today - Jan 31
+        </h2>
+        <h2 className="text-md flex flex-row gap-3">
+          <p className="font-bold">Practice Session:</p> Mon Feb 10 16:00 IT210
+        </h2>
+        <h2 className="text-md flex flex-row gap-3">
+          <p className="font-bold">Competition Session:</p> Sat Feb 15 13:00
+          IT210
+        </h2>
+      </div>
+
+      {/* Launcher Bar */}
+      <div className="absolute bottom-3 lg:bottom-0 rounded-3xl lg:rounded-none lg:left-0 lg:h-full flex flex-row lg:flex-col max-w-full items-center lg:space-y-1 lg:pt-6 bg-gray-800 bg-opacity-65  shadow-lg">
+        <AppIcon name="Home" href="/" icon={<span>🏠</span>} />
+        <AppIcon name="Register" href="/regis" icon={<span>📝</span>} />
+        <AppIcon name="Help" href="/contact" icon={<span>❓</span>} />
+      </div>
+
+      <footer className="absolute bottom-36 lg:bottom-2 text-xs text-gray-500">
+        <p>
+          Copyright © 2025. Dev Club, in association with Faculty of Information
+          and Communication Technology, Mahidol University. All rights reserved.
+        </p>
       </footer>
     </div>
   );
